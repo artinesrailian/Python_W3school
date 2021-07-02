f6412fcd5a5c13ef54a9c13dc0906488da605137
Artin.Esrailian <artine@energizeglobal.com>
2021-07-02 12:02:05 +0400

    Add some more examples to the 1-HelloWorld.py file

--- 1-HelloWorld.py
+++ 1-HelloWorld.py
@@ -1,1 +1,10 @@
+#This is the hello wolrd example
 print("Hello, World!")
+
+#This is the indention example
+if 5 > 2:
+    print("Five is greater than two!")
+
+#This is variable types example
+x = 5
+y = "Hello, World!"
